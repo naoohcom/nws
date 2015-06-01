7b1fc5b09abff7e7dc565fff391f29a4356a47f3
윤찬 <encephaloma@naooh.com>
2015-06-02 00:18:32 +0900

    worksheet data json 포맷을 변경, 사이드메뉴가 스크롤을 따라다님, 대시보드 데이터 규격 생성중.

--- app/scripts/worksheet.js
+++ app/scripts/worksheet.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('nWorkSheet', ['ngTable'])
 
-	.controller('nworksheetCtrl', function($scope, ngTableParams, $filter)
+	.controller('nworksheetCtrl', function()
 	{ })
 	.directive('nws', function($http, ngTableParams){
 		return{
@@ -14,21 +14,22 @@ angular.module('nWorkSheet', ['ngTable'])
 			link : function(scope)
 			{
 
+				scope.dashboardData = {}; // 대시보드용 데이터
+				scope.groupBy = 'category'; // 그룹화 시킬 필터 데이터
+				scope.categoryName = '전체'; // 기본 카테고리
 
-				scope.selectedCategory = '';
-				scope.groupBy = 'category';
-				scope.categoryName = '전체';
-
-				scope.tableParams = new ngTableParams({
-					page: 1,            // show first page
-					count: 10           // count per page
-				});
+				/**
+				 * http 웹 주소 규칙에 맞는지 테스트 한다.
+				 * @param text {string}
+				 * @returns http 양식 정규식에 맞는지 테스트 후 반환되는 값 {boolean}
+				 */
 				scope.httpreg = function(text){
 					var re = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])/;
 					return re.test(text);
 				};
 
-				if(scope.nwsDataUrl !== undefined)
+				// nwsDataUrl 가 undefined 이거나, worksheetParams 가 undefined 면 다음을 실행한다.
+				if(scope.nwsDataUrl !== undefined && scope.worksheetParams === undefined)
 				{
 					$http.get(scope.nwsDataUrl).success(function(data)
 					{
@@ -47,6 +48,77 @@ angular.module('nWorkSheet', ['ngTable'])
 						scope.$broadcast('dataLoaded', {type : 'all'});
 					});
 				}
+				scope.$on('dataLoaded', function()
+				{
+					(scope.dashboardData = function(rawData)
+					{
+						var catInfo = [],
+							totalInfo = {
+								count : 0,
+								done : 0,
+								average : 0,
+								undone : []
+							};
+
+						// count meta category and put data
+						for(var i = 0 ; i < rawData.meta.category.length ; i ++)
+						{
+
+							var itemToPut = {
+								name : rawData.meta.category[i]
+							};
+
+							var count = 0,
+								done = 0,
+								average = 0,
+								undone = [];
+							for(var j = 0 ; j < rawData.category.length; j ++)
+							{
+								for(var k = 0 ; k < rawData.category[j].data.length ; k++)
+								{
+									var item = rawData.category[j].data[k];
+									if(item.category === itemToPut.name)
+									{
+										count ++;
+										done += item.done;
+										//for(var undones = 0 ; unodnes < item.data.length)
+										if(item.state == '진행중')
+											undone.push(item.name);
+									}
+
+									//if(item === )
+								}
+							}
+							average = (done / (count));
+
+
+							$.extend(itemToPut, {
+								count : count,
+								done :done,
+								average : average,
+								undone : undone
+							});
+
+							catInfo.push(itemToPut);
+
+						}
+
+						for(var c = 0; c < catInfo.length ; c ++ )
+						{
+							totalInfo.count += catInfo[c].count;
+							totalInfo.done += catInfo[c].done;
+							$.extend(totalInfo.undone, catInfo[c].undone) ;
+						}
+						console.log(catInfo.length);
+						totalInfo.average = totalInfo.done/ totalInfo.count;
+
+						return{
+							totalInfo : totalInfo,
+							categoryInfo : catInfo
+						}
+					}(scope.worksheetParams));
+				});
+
 			},
 			controller : function()
 			{
@@ -56,14 +128,37 @@ angular.module('nWorkSheet', ['ngTable'])
 	.directive('nwsMenu', function($filter)
 	{
 		return{
-			template : '<div>' +
+			template : '<div style="position:relative;">' +
 							'<ul >' +
 								'<li ng-click="changeCategory()"> <a>전체</a></li>' +
 								'<li ng-click="changeCategory(item.name)" ng-repeat="item in worksheetParams.category"><a style="cursor:pointer;">{{ item.name }}</a></li>' +
 							'</ul>' +
 						'</div>',
 			require : '^nws',
-			link : function(scope){
+			link : function(scope, element){
+
+				$(window).on('scroll', function(){
+					var margin = 20;
+					var target = $(element);
+					var position = target.offset().top - $(window).scrollTop()  - margin;
+					console.log(position);
+					if(position <= 0 )
+					{
+						target.find('ul').css({
+							position : 'fixed',
+							top : margin
+						});
+
+					}else
+					{
+
+						target.find('ul').css({
+							position : 'inherit',
+							top : 'inherit'
+						});
+					}
+				});
+
 				scope.changeCategory = function(name)
 				{
 
